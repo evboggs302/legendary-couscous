4689a74248f4ba121cb4299169b0bd7fd04b3ce0
evboggs302 <evboggs94@gmail.com>
2021-12-09 23:24:41 -0500

    wrapping up work on modal

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("build"));
+app.listen(port, () => console.log("listening on port#: ", port));
